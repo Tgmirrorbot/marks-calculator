5c27e93bcc6b239c323a7a6445655e6ec62dbb84
Tgmirrorbot <107290462+Tgmirrorbot@users.noreply.github.com>
2023-11-08 17:04:33 +0530

    Commit from Stub AI (#3)
    Co-authored-by: Tgmirrorbot <ai@codestub.ai>

--- components/SubjectForm.tsx
+++ components/SubjectForm.tsx
@@ -39,39 +39,43 @@ const SubjectForm = () => {
   return (
     <div>
       {subjects.map((subject, index) => (
-        <div key={index}>
+        <div key={index} className="flex flex-col space-y-2">
+          <label className="text-gray-600">Subject Name</label>
           <input
-            className="text-black"
+            className="border-2 border-gray-200 rounded-md p-2"
             value={subject.name}
             onChange={(e) => updateSubject(index, 'name', e.target.value)}
             placeholder="Subject Name"
           />
+          <label className="text-gray-600">Final Marks</label>
           <input
-            className="text-black"
+            className="border-2 border-gray-200 rounded-md p-2"
             type="number"
             value={subject.finalMarks}
             onChange={(e) => updateSubject(index, 'finalMarks', Number(e.target.value))}
             placeholder="Final Marks"
           />
+          <label className="text-gray-600">Internal Marks</label>
           <input
-            className="text-black"
+            className="border-2 border-gray-200 rounded-md p-2"
             type="number"
             value={subject.internalMarks}
             onChange={(e) => updateSubject(index, 'internalMarks', Number(e.target.value))}
             placeholder="Internal Marks"
           />
+          <label className="text-gray-600">Midterm Marks</label>
           <input
-            className="text-black"
+            className="border-2 border-gray-200 rounded-md p-2"
             type="number"
             value={subject.midtermMarks}
             onChange={(e) => updateSubject(index, 'midtermMarks', Number(e.target.value))}
             placeholder="Midterm Marks"
           />
-          <button onClick={() => removeSubject(index)}>Remove</button>
+          <button className="bg-red-500 text-white rounded-md p-2 mt-2" onClick={() => removeSubject(index)}>Remove</button>
           <p>Grade: {calculateGrade(subject)}</p>
         </div>
       ))}
-      <button onClick={addSubject}>Add Subject</button>
+      <button className="bg-blue-500 text-white rounded-md p-2 mt-2" onClick={addSubject}>Add Subject</button>
     </div>
   );
 };
