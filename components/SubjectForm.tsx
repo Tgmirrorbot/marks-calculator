937d1775c93d1fd03e994d167e29f524a84a11e7
Tgmirrorbot <107290462+Tgmirrorbot@users.noreply.github.com>
2023-11-08 17:15:35 +0530

    Commit from Stub AI (#5)
    Co-authored-by: Tgmirrorbot <ai@codestub.ai>

--- components/SubjectForm.tsx
+++ components/SubjectForm.tsx
@@ -28,7 +28,7 @@ const SubjectForm = () => {
 
   const calculateGrade = (subject: Subject) => {
     const totalMarks = (subject.finalMarks / 2) + (subject.midtermMarks / 2) + subject.internalMarks;
-    if (totalMarks < 40) return 'F';
+    if (totalMarks <= 40) return 'Pass';
     if (totalMarks < 51) return 'D';
     if (totalMarks < 61) return 'C';
     if (totalMarks < 71) return 'B';
@@ -37,45 +37,70 @@ const SubjectForm = () => {
   };
 
   return (
-    <div>
-      {subjects.map((subject, index) => (
-        <div key={index} className="flex flex-col space-y-2">
-          <label className="text-gray-600">Subject Name</label>
-          <input
-            className="border-2 border-gray-200 rounded-md p-2 text-black"
-            value={subject.name}
-            onChange={(e) => updateSubject(index, 'name', e.target.value)}
-            placeholder="Subject Name"
-          />
-          <label className="text-gray-600">Final Marks</label>
-          <input
-            className="border-2 border-gray-200 rounded-md p-2 text-black"
-            type="number"
-            value={subject.finalMarks}
-            onChange={(e) => updateSubject(index, 'finalMarks', Number(e.target.value))}
-            placeholder="Final Marks"
-          />
-          <label className="text-gray-600">Internal Marks</label>
-          <input
-            className="border-2 border-gray-200 rounded-md p-2 text-black"
-            type="number"
-            value={subject.internalMarks}
-            onChange={(e) => updateSubject(index, 'internalMarks', Number(e.target.value))}
-            placeholder="Internal Marks"
-          />
-          <label className="text-gray-600">Midterm Marks</label>
-          <input
-            className="border-2 border-gray-200 rounded-md p-2 text-black"
-            type="number"
-            value={subject.midtermMarks}
-            onChange={(e) => updateSubject(index, 'midtermMarks', Number(e.target.value))}
-            placeholder="Midterm Marks"
-          />
-          <button className="bg-red-500 text-white rounded-md p-2 mt-2" onClick={() => removeSubject(index)}>Remove</button>
-          <p>Grade: {calculateGrade(subject)}</p>
-        </div>
-      ))}
-      <button className="bg-blue-500 text-white rounded-md p-2 mt-2" onClick={addSubject}>Add Subject</button>
+    <div className="flex">
+      <div className="w-1/2 p-4">
+        <button className="bg-blue-500 text-white rounded-md p-2 mt-2" onClick={addSubject}>Add Subject</button>
+        {subjects.map((subject, index) => (
+          <div key={index} className="flex flex-col space-y-2 mt-4">
+            <label className="text-gray-600">Subject Name</label>
+            <input
+              className="border-2 border-gray-200 rounded-md p-2 text-black"
+              value={subject.name}
+              onChange={(e) => updateSubject(index, 'name', e.target.value)}
+              placeholder="Subject Name"
+            />
+            <label className="text-gray-600">Final Marks</label>
+            <input
+              className="border-2 border-gray-200 rounded-md p-2 text-black"
+              type="number"
+              value={subject.finalMarks}
+              onChange={(e) => updateSubject(index, 'finalMarks', Number(e.target.value))}
+              placeholder="Final Marks"
+            />
+            <label className="text-gray-600">Internal Marks</label>
+            <input
+              className="border-2 border-gray-200 rounded-md p-2 text-black"
+              type="number"
+              value={subject.internalMarks}
+              onChange={(e) => updateSubject(index, 'internalMarks', Number(e.target.value))}
+              placeholder="Internal Marks"
+            />
+            <label className="text-gray-600">Midterm Marks</label>
+            <input
+              className="border-2 border-gray-200 rounded-md p-2 text-black"
+              type="number"
+              value={subject.midtermMarks}
+              onChange={(e) => updateSubject(index, 'midtermMarks', Number(e.target.value))}
+              placeholder="Midterm Marks"
+            />
+            <button className="bg-red-500 text-white rounded-md p-2 mt-2" onClick={() => removeSubject(index)}>Remove</button>
+          </div>
+        ))}
+      </div>
+      <div className="w-1/2 p-4">
+        <table className="table-auto w-full">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">Subject Name</th>
+              <th className="px-4 py-2">Final Marks</th>
+              <th className="px-4 py-2">Internal Marks</th>
+              <th className="px-4 py-2">Midterm Marks</th>
+              <th className="px-4 py-2">Grade</th>
+            </tr>
+          </thead>
+          <tbody>
+            {subjects.map((subject, index) => (
+              <tr key={index}>
+                <td className="border px-4 py-2">{subject.name}</td>
+                <td className="border px-4 py-2">{subject.finalMarks}</td>
+                <td className="border px-4 py-2">{subject.internalMarks}</td>
+                <td className="border px-4 py-2">{subject.midtermMarks}</td>
+                <td className="border px-4 py-2">{calculateGrade(subject)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
